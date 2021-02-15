4e7d106efa796b1f572724d50d639c696be64a02
Brian Nkuru <nkurunungib@gmail.com>
2021-02-15 10:58:31 -0600

    SSO Set up with FB

---
+++ src/main/java/com/crystal/ssfbauthentication/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.crystal.ssfbauthentication.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+    @GetMapping("/")
+    public String login(){
+        return "index";
+    }
+}
---
+++ src/test/java/com/crystal/ssfbauthentication/SsFbAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crystal.ssfbauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsFbAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
